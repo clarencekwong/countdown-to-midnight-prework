2e7378128be00e1fac46b4c2b7cd75e0fa16a74e
Clarence Kwong <clarencekwong7@gmail.com>
2018-11-17 19:58:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(number)
   counter = 0 
-  while number < counter
+  while number < counter do
     puts "#{number} SECONDS(S)!"
     number -= 1
   end
-  
+  puts "HAPPY NEW YEAR!"
 end
